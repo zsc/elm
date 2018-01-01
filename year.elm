350f7c68ce18a7c8434bd91319de79886c93bfc3
Shuchang Zhou <zsc@Shuchangs-MacBook-Pro.local>
2018-01-01 09:36:34 +0800

    Add github ribbon

--- year.elm
+++ year.elm
@@ -1,4 +1,4 @@
-import Html exposing (Attribute, div, text, input, img, br, button)
+import Html exposing (Attribute, div, text, input, img, br, button, a)
 import Html.Attributes as H exposing (..)
 import Html.Events exposing (on, onInput, onClick)
 import Json.Decode exposing (string, map)
@@ -25,9 +25,11 @@ view model =
   let style_ x = if model.year == x then style [("fontWeight", "bold")] else style []
   in
   div []
-    ([img [src (Tuple.first model.url), width 200] []
-    , br [] []
+    ([
+    a [href "https://github.com/zsc/elm"] [img [style [("position", "absolute"), ("top", "0"), ("left", "0"), ("border", "0")], src "https://camo.githubusercontent.com/121cd7cbdc3e4855075ea8b558508b91ac463ac2/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f677265656e5f3030373230302e706e67"] []]
     , img [src (Tuple.second model.url), height 400] []
+    --, br [] []
+    , img [src (Tuple.first model.url), width 200] []
     , br [] []
 --    , input
 --      [ type_ "range"
