dae3962a4febd6f1f47a74cb519303b9d3021bac
Shuchang Zhou <zsc@Shuchangs-MacBook-Pro.local>
2018-12-16 07:57:40 +0800

    [arith] Show the slowest expr's.

--- elm_0.19/src/arith.elm
+++ elm_0.19/src/arith.elm
@@ -24,14 +24,6 @@ main =
 
 -- MODEL
 
-stat : List Time.Posix -> String
-stat clicks =
-    if List.length clicks < 2 then "Click \"Next\"" else
-        let cs = List.map Time.posixToMillis clicks in
-        let delta = (Maybe.withDefault 0 (List.head cs)) - (Maybe.withDefault 0 (List.head (List.drop (List.length cs - 1) cs))) in
-        let qps = round (60000.0 / (toFloat delta) * (toFloat (List.length cs - 1))) in
-        "Total: " ++ String.fromInt (List.length cs) ++ ", per minute: " ++ String.fromInt qps
-
 type Expr
  = Plus Int Int
  | Minus Int Int
@@ -61,10 +53,16 @@ rand =
                      Random.uniform expr []
             )
 
+type alias Click = 
+  { time : Time.Posix
+  , expr : Expr
+  }
+
+
 type alias Model =
   { dieFace : Expr
   , time : Time.Posix
-  , clicks : List Time.Posix
+  , clicks : List Click
   }
 
 
@@ -74,7 +72,21 @@ init _ =
   , Cmd.none
   )
 
+worst : List Click -> List (Html msg)
+worst clicks =
+  if List.length clicks < 2 then [] else
+      let except_last = List.take (List.length clicks - 1) clicks in
+      let diffs = List.map2 (\a b -> (a.expr, Time.posixToMillis a.time - Time.posixToMillis b.time)) except_last (List.drop 1 clicks) in
+      let top = List.take 3 (List.reverse (List.sortBy (\(a,b) -> b) diffs)) in
+      (List.map (\(a, b) -> text (String.fromInt (round (toFloat b / 100.0)) ++ ", " ++ repr a)) top)
 
+stat : List Click -> String
+stat clicks =
+    if List.length clicks < 2 then "Click \"Next\"" else
+        let cs = List.map (\h -> Time.posixToMillis h.time) clicks in
+        let delta = (Maybe.withDefault 0 (List.head cs)) - (Maybe.withDefault 0 (List.head (List.drop (List.length cs - 1) cs))) in
+        let qps = round (60000.0 / (toFloat delta) * (toFloat (List.length cs - 1))) in
+        "Total: " ++ String.fromInt (List.length cs) ++ ", per minute: " ++ String.fromInt qps
 
 -- UPDATE
 
@@ -89,7 +101,7 @@ update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
     Roll ->
-      ( { model | clicks = model.time :: model.clicks}
+      ( { model | clicks = {time = model.time, expr = model.dieFace} :: model.clicks}
       , Random.generate NewFace rand
       )
 
@@ -119,8 +131,9 @@ buttonN = button [ onClick Roll ] [div [style "font-size" "32px", style "height"
 view : Model -> Html Msg
 view model =
   div []
-    [ div [style "font-size" "64px", style "text-align" "center"] 
+    ([ div [style "font-size" "64px", style "text-align" "center"] 
           [ text (repr model.dieFace) ]
     , div [style "text-align" "center"] [buttonN]
-    , div [style "font-size" "32px"] [ text ((stat model.clicks)) ]
-    ]
+    , div [style "font-size" "32px"] [ text (stat model.clicks)]
+    , br [] []
+    ] ++ (List.map (\x -> div [style "font-size" "32px"] [x]) (worst model.clicks)))
