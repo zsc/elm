cf3656b887d9f7e6ea8758dcbd2c73c5032b3efc
Shuchang Zhou <shuchang.zhou@gmail.com>
2019-05-02 06:49:31 +0800

    [word2vec] Init version.

---
+++ word2vec/src/word2vec.elm
@@ -0,0 +1,67 @@
+import Browser
+import Html exposing (Html, Attribute, div, input, text)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
+import Maybe
+
+
+
+-- MAIN
+
+
+main =
+  Browser.sandbox { init = init, update = update, view = view }
+
+
+
+-- MODEL
+
+
+type alias Model =
+  { content : String
+  , content2 : String
+  , content3 : String
+  }
+
+
+init : Model
+init =
+  { content = "", content2 = "", content3 = "" }
+
+interpolate x y z = "unknown" -- x + y - z
+
+-- UPDATE
+
+
+type Msg
+  = Change String
+  | Change2 String
+  | Change3 String
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    Change newContent ->
+      { model | content = newContent }
+    Change2 newContent ->
+      { model | content2 = newContent }
+    Change3 newContent ->
+      { model | content3 = newContent }
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+  div [style "font-size" "48px"]
+    [ input [ placeholder "Input a word", value model.content, onInput Change ] []
+    , div [style "font-size" "24px", style "display" "inline"] [text " - "]
+    , input [ placeholder "Input a word", value model.content2, onInput Change2 ] []
+    , div [] []
+    , div [style "font-size" "16px", style "display" "inline"] [text " + "]
+    , input [ placeholder "Input a word", value model.content3, onInput Change3 ] []
+    , div [] []
+    , div [style "font-size" "24px", style "display" "inline"] [text " = ", text (
+        interpolate model.content model.content2 model.content3
+        ) ]
+    ]
