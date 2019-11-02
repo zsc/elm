8b4649acfaf5313931fa26ccb1ce1e60b130bf98
Shuchang Zhou <shuchang.zhou@gmail.com>
2019-11-02 18:32:42 +0800

    Glossary app.

---
+++ glossary/src/main.elm
@@ -0,0 +1,99 @@
+import Set
+import Browser
+import Html exposing (..)
+import Html.Events exposing (..)
+import Html.Attributes exposing ( style, placeholder, src, width, href, value , rows)
+import List
+import Maybe
+import Random
+import Time
+import Data exposing (..)
+
+
+-- MAIN
+
+main =
+  Browser.element
+    { init = init
+    , update = update
+    , subscriptions = subscriptions
+    , view = view
+    }
+
+
+-- MODEL
+
+type alias Model =
+  { field : String
+  , explain : List String
+  , level : Int
+  }
+
+fromJust : Maybe a -> a
+fromJust m_a = case m_a of
+  Just v -> v
+  Nothing -> Debug.todo "fromJust"
+
+init : () -> (Model, Cmd Msg)
+init _ =
+  ( { field = ""
+    , explain = []
+    , level = 1
+    }
+  , Cmd.none
+  )
+
+-- UPDATE
+
+
+type Msg
+  = Roll
+  | ChangeLevel Int
+  | Change String
+  | Tick Time.Posix
+
+update_explain str = List.filter (String.contains str) my_list
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    ChangeLevel lv ->
+      ( {model | level = lv}
+      , Cmd.none
+      )
+    Change value ->
+      ( {model | field = value, explain = update_explain value}
+      , Cmd.none
+      )
+    _ -> Debug.todo ""
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Time.every 100 Tick
+
+
+-- VIEW
+
+levelDescription level = case level of
+  1 -> "词汇表"
+  2 -> "矩阵减"
+  3 -> "矩阵乘"
+  _ -> Debug.todo "Unknown level"
+
+levelButtons curLevel = List.map
+  (\l -> button ([onClick (ChangeLevel l), style "font-size" "24px"] ++ (if curLevel == l then [style "font-weight" "bold"] else []))
+                [text (levelDescription l)])
+  [1]
+
+view : Model -> Html Msg
+view model =
+  div []
+    ([ div [style "text-align" "center"] [a [style "font-size" "24px", href "https://zsc.github.io/637913017.jpg"] [text "打赏"]]
+    , div [style "font-size" "32px"] [text "　"]
+    , div [style "text-align" "center"] (levelButtons model.level)
+    , div [style "font-size" "32px"] [input [ placeholder "What to lookup?", value model.field, onInput Change ] []]
+    ] ++ List.map (\x -> div [style "font-size" "24px"] [text x]) model.explain
+    )
