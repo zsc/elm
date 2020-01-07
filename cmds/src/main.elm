963dc1b21c93cbc8f8c640d4d666cd8b4802a929
Shuchang Zhou <shuchang.zhou@gmail.com>
2020-01-08 07:25:49 +0800

    Add json.

---
+++ cmds/src/main.elm
@@ -0,0 +1,149 @@
+import Set
+import Browser
+import Html exposing (..)
+import Html.Events exposing (..)
+import Html.Attributes exposing ( style, placeholder, src, width, href, value , rows, cols)
+import List
+import Maybe
+import Random
+import Time
+import Json.Decode
+
+
+-- MAIN
+
+main =
+  Browser.element
+    { init = init
+    , update = update
+    , subscriptions = subscriptions
+    , view = view
+    }
+
+
+-- MODEL
+
+type alias Model =
+  { fieldA : String
+  , fieldB : String
+  , level : Int
+  }
+
+fromJust : Maybe a -> a
+fromJust m_a = case m_a of
+  Just v -> v
+  Nothing -> Debug.todo "fromJust"
+
+sampleIn = """{
+    "cmd": "sync",
+    "id": 1,
+    "ids": [
+        [
+            [
+                0
+            ],
+            "ld",
+            0
+        ]
+    ],
+    "task_id": 0
+}"""
+
+sampleOut = """[details = "1  , sync"]
+```
+{
+    "cmd": "sync",
+    "id": 1,
+    "ids": [
+        [
+            [
+                0
+            ],
+            "ld",
+            0
+        ]
+    ],
+    "task_id": 0
+}
+```
+[/details]"""
+
+init : () -> (Model, Cmd Msg)
+init _ =
+  ( { fieldA = sampleIn
+    , fieldB = sampleOut
+    , level = 1
+    }
+  , Cmd.none
+  )
+
+-- UPDATE
+
+
+type Msg
+  = Roll
+  | ChangeLevel Int
+  | ChangeA String
+  | ChangeB String
+  | Tick Time.Posix
+
+processString : String -> String
+processString x = x ++ x
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    ChangeLevel lv ->
+      ( {model | level = lv}
+      , Cmd.none
+      )
+    ChangeA value ->
+      ( {model | fieldA = value, fieldB = processString value}
+      , Cmd.none
+      )
+    ChangeB value ->
+      ( model 
+      , Cmd.none
+      )
+    _ -> Debug.todo ""
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Time.every 100 Tick
+
+
+-- VIEW
+
+levelDescription level = case level of
+  1 -> "论坛格式"
+  2 -> "矩阵减"
+  3 -> "矩阵乘"
+  _ -> Debug.todo "Unknown level"
+
+levelButtons curLevel = List.map
+  (\l -> button ([onClick (ChangeLevel l), style "font-size" "24px"] ++ (if curLevel == l then [style "font-weight" "bold"] else []))
+                [text (levelDescription l)])
+  [1]
+
+view : Model -> Html Msg
+view model =
+  div []
+    ([ div [style "text-align" "center"] [a [style "font-size" "24px", href "https://zsc.github.io/637913017.jpg"] [text "打赏"]]
+    , div [style "font-size" "32px"] [text "　"]
+    , div [style "text-align" "center"] (levelButtons model.level)
+    --, div [style "font-size" "32px", style "text-align" "center"]
+    --      [ text ("功能 " ++ String.fromInt model.level ++ ": " ++ levelDescription model.level)]
+    --, div [style "font-size" "24px"] [text "名单A"]
+    , div [style "font-size" "32px"] ([textarea [cols 60, rows (max 10 (List.length (String.split "\n" model.fieldA))), placeholder "名单A", value (model.fieldA) , onInput ChangeA] []])
+    , div [style "font-size" "16px"] [textarea [cols 60, rows 20, value model.fieldB, onInput ChangeB] []]
+    --, div [style "font-size" "24px"] [text "名单B"]
+    --, div [style "font-size" "32px"] ()
+    --, div [style "font-size" "24px"] [text "名单A - 名单B"]
+    --, div [style "font-size" "16px"] [text (diff_string model.fieldA model.fieldB)]
+    --, div [style "font-size" "24px"] [text "名单B - 名单A"]
+    --, div [style "font-size" "16px"] [text (diff_string model.fieldB model.fieldA)]
+    ]
+    )
